7bb4caef4fbed7faf5e44f498068e90466f2066b
Elnely1 <https://github.com/Elnely1>
2024-04-17 01:35:09 +0200

    4th

---
+++ lesson4.py
@@ -0,0 +1,9 @@
+text = "Elnely\none"
+print(len(text))                 #count the number of char
+print(text[3])                  #know the char in this position
+print(text.index("e"))          #know the position of this char
+print(text.replace("ne","nee"))
+#name of string.lower--->make all char is small
+#______________.upper--->make all char is captal
+#______________.islower--->check if all char is small
+#______________.isupper--->check all char is captal
