2a6cd3cdb55e0608db5a573a1522beff597db0e7
Elnely1 <https://github.com/Elnely1>
2024-04-17 01:00:01 +0200

    3rd

--- lesson3.py
+++ lesson3.py
@@ -1,6 +1,12 @@
 character_name = "Elnely"
+age = "15"
+is_male = True
 print("There is a man called " + character_name )
 print(character_name + " has no brothers")
 print("He is a good man")
+print("His age is " + age)
+character_name = "Mo"
+age = "50"
 print("He is an engineering student")
-print("Thank you")
+print("HIs father age is " + age)
+print("Thank you " + character_name)
