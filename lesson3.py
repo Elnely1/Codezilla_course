b40547f5fcc699a3a05c932a70153d81d7d2aa99
Elnely1 <https://github.com/Elnely1>
2024-04-17 00:48:49 +0200

    3rd

---
+++ lesson3.py
@@ -0,0 +1,5 @@
+character_name = "Elnely"
+print("There is a man called " + character_name )
+print(character_name + " has no brothers")
+print("He is a good man")
+print("He is an engineering student")
