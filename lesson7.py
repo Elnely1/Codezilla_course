f971b3714e61140f8fad843956ecbfbe86d358df
Elnely1 <https://github.com/Elnely1>
2024-04-19 18:46:52 +0200

    7th

---
+++ lesson7.py
@@ -0,0 +1,6 @@
+num1 = input("enter the first number: ")
+num2 = input("enter the second number: ")
+#result1 = int(num1)+int(num2)
+result2 = float(num1)+float(num2)      #to non int numbers
+#print(result1)
+print(result2)
