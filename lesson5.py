bdb46b0a481bf8ba89151f99af628425467bb821
Elnely1 <https://github.com/Elnely1>
2024-04-19 17:03:17 +0200

    5th

---
+++ lesson5.py
@@ -0,0 +1,13 @@
+from math import *
+num = 15
+print (num)
+print(str(num) + " is my age")  #--->make the number a string to print it witht the srings
+print(abs(-10))
+print(pow(2,3))
+print(max(5,6))
+print(min(9,4))
+print(round(6.8))   #--->make Approximation of the number
+#then i need to import the math lib
+print(floor(6.8))   #--->get me lowest the int to the number
+print(ceil(6.8))    #--->get me the higher int to the number
+print(sqrt(16))     #--->make square root
